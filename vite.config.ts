44c1593595bcd949172c76a66b1c8334904ab311
Websprint <websprintt@gmail.com>
2026-05-10 17:09:03 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,8 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Seguridad-TISA/',
+    base: './',
     plugins: [react(), tailwindcss()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),

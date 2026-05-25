360f4a93e5aed727f0945063101e11fd41f1e54a
Websprint <websprintt@gmail.com>
2026-05-25 17:00:28 +0200

    Update solutions.ts

--- src/data/solutions.ts
+++ src/data/solutions.ts
@@ -46,7 +46,7 @@ export const solutionCategories: SolutionCategory[] = [
           "Batería o cableado: Máxima versatilidad para instalarlo en cinco minutos en cualquier tipo de fachada."
         ],
         whyRecommend: "Es la opción más eficiente y moderna para el control de accesos. Elimina por completo los costes ocultos de otras marcas y ofrece una cobertura de seguridad en la puerta principal que ningún videoportero de una sola lente puede igualar.",
-        amazonUrl: "https://www.amazon.es/s?k=Eufy+Video+Doorbell+E340",
+        amazonUrl: "https://www.amazon.es/eufy-Security-Almacenamiento-Ampliable-mensuales/dp/B0CF8RPX5M?dib=eyJ2IjoiMSJ9.rsSlJUgNwp0c8iwr_MtjP3EvjEDRQZiKI_X9hJizlwGDxvt4kFmfbj4ZZwDqnopauEaVyEUar8Ek7LwqtEGUJ4dhH52feWTwmYgGPScIY7gL4RCd5sBfBb6K6iMTpI4hiRMlNlv8-Txs8jsOvnguzi6gcS7X2RQTYkoFhfcOfbsX9xYEFGUSNTdN6KfQTq8UuCjmB-pc_785zDvfy1M6pXEb6HBq_634wXz2GprqkLjfZG2JluhkAOvg5QDtedFLXYyG44F8iVErrwv1bTbdIBAsN6ClNYsjWNLRmmB21fA.o5bAJjDhloKQ8aqYkhoBDqSKSK3VO4nmMIPoAQrolkw&dib_tag=se&keywords=Eufy%2BVideo%2BDoorbell%2BE340&qid=1779720278&sr=8-1&ufe=app_do%3Aamzn1.fos.4c3f56c3-e485-4a35-9abc-6532b61c3b62&th=1&linkCode=ll2&tag=tisaseguridad-21&linkId=d37095381c14098ab19f339ae69a7e80&ref_=as_li_ss_tl",
         image: "https://m.media-amazon.com/images/I/61Sfuf3fyEL._AC_SL1500_.jpg",
         category: "Videoportero"
       },
@@ -60,7 +60,7 @@ export const solutionCategories: SolutionCategory[] = [
           "Hub con sirena integrada: El puente de conexión actúa también como una alarma sonora que se activa si detecta un intruso."
         ],
         whyRecommend: "A diferencia de los sensores WiFi tradicionales que agotan las pilas en meses y sufren retrasos, este sistema es profesional, ultra-rápido y sirve como una alarma doméstica escalable y sin cuotas.",
-        amazonUrl: "https://www.amazon.es/s?k=TP-Link+Tapo+T110+Hub+H100",
+        amazonUrl: "https://www.amazon.es/Bundle-TP-Link-Sensor-Contacto-Inteligente/dp/B0C25173HH?dib=eyJ2IjoiMSJ9.FAqU-4c0nGT1beIcuvg1JFv8wh-2c1p73bjRd3ZpkrSXVicHoW6Ms2Op-9TRngOq3OyWzuBw_mGDiN6n08O2YhkcCGKoW-NvGv1acOrKI97sl-xPvbq7lD367G9CXoC_-TLxhoVR44iQ52rx8OfVyqKzsT96YDX0e03Gphh802OTBcfDQ7tLaU1iZbsmUX-9sch54uC5slyMuReqpEZkZOmF_QzbrG7pCuZW4mWkqQywE0ShhwAZGDlUUwmIT9xzOOHqE5ee6_WuuAmJssSNAl-65mvuOZpdnU2ZyFXROKI.eW1u_MUuRsbyAsg5bywhfB1WhY0NzuSbC86tDHBIAoA&dib_tag=se&keywords=TP-Link+Tapo+T110+Hub+H100&qid=1779720335&sr=8-2&linkCode=ll2&tag=tisaseguridad-21&linkId=c73943775210c1dc44915142ca3e75a7&ref_=as_li_ss_tl",
         image: "https://m.media-amazon.com/images/I/71MhoffWqyL._AC_SL1500_.jpg",
         category: "Sensores"
       },
@@ -74,7 +74,7 @@ export const solutionCategories: SolutionCategory[] = [
           "Seguimiento automatizado 360°: Detecta el movimiento y rota sola para rastrear al objetivo por todo el jardín o fachada."
         ],
         whyRecommend: "Es el producto estrella para el exterior del hogar. Reúne resolución 3K, almacenamiento local gratuito, rotación total y autosuficiencia energética en un solo dispositivo. No existe una alternativa más moderna y eficiente.",
-        amazonUrl: "https://www.amazon.es/s?k=eufy+SoloCam+S340",
+        amazonUrl: "https://www.amazon.es/eufy-Security-SoloCam-S340-inal%C3%A1mbrica/dp/B0CNVHDVQ3?dib=eyJ2IjoiMSJ9.VPqxDWLl9OeZrpAvQwyAXnzGiJdN4XLJ_A7tDey7jtkncFRbb3qkbsXmIFi5ZW-uMWjEly4BiRNndRgUR6OX4Odk8S2KuqUWGESdMqND6tB3XczpU3kTDzG-vF_bE8rlAIXdtSNiCl9NpsBPL3xXWqQOdUiK-XWDHXabw80w7f8yS-hmxHDsXGWhilv2BbUqvKIEB4FW8lzrcWXBExVZCmC45mwJZuG59_0A77r6rBCVSGdfP2SHZe6O4aZ08XV7isJmowjYwQikwIgJ_hxD3BM9or8llm8B81X3YDzJ7qQ.Ca0QwZQnhj2MUQgWtH4WEF7OJtfOlBG8OJrYbo3ZVww&dib_tag=se&keywords=eufy+SoloCam+S340&qid=1779720398&sr=8-9&linkCode=ll2&tag=tisaseguridad-21&linkId=285c8e1803d3781b45c4a12485b4e96a&ref_=as_li_ss_tl",
         image: "https://m.media-amazon.com/images/I/61f+qXLtQcL._AC_SL1500_.jpg",
         category: "Cámara Exterior"
       },
@@ -88,7 +88,7 @@ export const solutionCategories: SolutionCategory[] = [
           "Extra de Brillo (1055 Lúmenes): Al ser más potentes que las bombillas estándar, la luz se nota perfectamente desde la calle incluso con las persianas semi-bajadas."
         ],
         whyRecommend: "La simulación de presencia es la forma más barata y efectiva de disuadir a los ladrones. Este modelo específico de Tapo ofrece la máxima fiabilidad tecnológica actual, es muy brillante y cuesta una fracción del precio de los sistemas antiguos que requerían comprar costosas centralitas aparte.",
-        amazonUrl: "https://www.amazon.es/s?k=TP-Link+Tapo+L535E",
+        amazonUrl: "https://www.amazon.es/Tapo-L535E-inteligentes-equivalente-certificado/dp/B0CKXSYYH2?dib=eyJ2IjoiMSJ9.KpgWlAHTLO5YEYC9ton1-UhGbDCslyb6a1bV4Q48aP9fMue13lZiludbk8QoEQCU_xlDTAcMgjMTm9wHALnhDZ545H-8cRoqfWN7Ih2acONDn51RBvSYbmVoAR0liHyfrbymxrPkbeXVj_sG1DhE_xpbEDde6eN1Hdbdlgulyf9VzqexG8ASgU_vNxPa7akPmcJfCjtPAiiHFZnmCd8KGwR7v00ebU71H8sR1l3YzL_7uLGEUEhI_HiTxGYiFsqIWp8km8OamlgHTylOilMpkMaMePTvVA3bQu5wYEkgSmw.KFX5JoHHTcJy_jTqPNNWLqdX-Q9-WRb2zlCzeFrwpQE&dib_tag=se&keywords=TP-Link%2BTapo%2BL535E&qid=1779720458&sr=8-1&th=1&linkCode=ll2&tag=tisaseguridad-21&linkId=ad2ee3ca59b76b0332d096d6a8280414&ref_=as_li_ss_tl",
         image: "https://m.media-amazon.com/images/I/6155xjQxhfL._AC_SL1500_.jpg",
         category: "Simulación"
       },
@@ -102,7 +102,7 @@ export const solutionCategories: SolutionCategory[] = [
           "Control por voz y táctil: Mira tus cámaras diciendo 'Alexa, enséñame el patio' o tocando la pantalla con los widgets directos del panel de control."
         ],
         whyRecommend: "Porque un hogar seguro necesita un cuadro de mandos centralizado. A diferencia de los altavoces Echo Show comunes que están pensados para el entretenimiento (música, fotos, recetas), el Echo Hub está diseñado exclusivamente para la domótica y la seguridad, ofreciendo la experiencia estética y funcional de una alarma profesional pero con total libertad y modernidad.",
-        amazonUrl: "https://www.amazon.es/s?k=Amazon+Echo+Hub",
+        amazonUrl: "https://www.amazon.es/echo-hub/dp/B0BCR8Z7QL?dib=eyJ2IjoiMSJ9.AitVZZFX-33h52IILcBAk9AASBJWBnQKvGFf36HQxi7jSypV9yrfIBLdvJABzfhZbEBk7PPVDzHEW_I_2HZXXd29R8anWqKWNvGDt12WLErw95U-wmmrRKBWHVsKzcT2k_oFHiopJ04Lntc5I08m3eUCG2OX_LwYcXIpc4jvZwORD4KTdtOUJa5Si0U5xQmYeulCJNI9gN7XvXEHvsyFUg.0neBk8A2PKTWSk0mV4sH16uS6yVGv5_NdHjBIW6HGaA&dib_tag=se&keywords=Amazon%2BEcho%2BHub&qid=1779720500&sr=8-1&ufe=app_do%3Aamzn1.fos.4c3f56c3-e485-4a35-9abc-6532b61c3b62&th=1&linkCode=ll2&tag=tisaseguridad-21&linkId=7061759fbd5c562393eed31ad06ec307&ref_=as_li_ss_tl",
         image: "https://m.media-amazon.com/images/I/61mR-D6SVJL._AC_SL1000_.jpg",
         category: "Control"
       }
@@ -123,7 +123,7 @@ export const solutionCategories: SolutionCategory[] = [
           "Inteligencia en Red: El NVR de 2TB gestiona las grabaciones de forma local y segura, permitiendo el acceso remoto encriptado desde el móvil del propietario."
         ],
         whyRecommend: "Considero que la estabilidad en un negocio no es negociable. El cableado PoE te garantiza que ninguna frecuencia WiFi inhibida dejará a tu local a ciegas. Su IA de detección humana es la más precisa en este rango de precio, ahorrándote la pesadilla de despertarte de madrugada por falsas alarmas provocadas por sombras o insectos.",
-        amazonUrl: "https://www.amazon.es/s?k=Reolink+RLK8-800B4",
+        amazonUrl: "https://www.amazon.es/Seguridad-Reolink-Exteriores-vigilancia-RLK8-800B4/dp/B07LBFJDQW?dib=eyJ2IjoiMSJ9.iMF3IL1YhbrKAkLwY0aQ_ZF1pp-f5kDHcB6PmBm1HecX76Uwl7nW0rh2eOajAU5TZxaFs2t6CnzrJJg-X-lLBeThEASF2fbquOEewwpkzUUOWxv4_f0xS1HPbd9dWRY8E3g3VnRcU4Qw3PRvu_jB27KLdnsgL7XBViOwiZ61Fyau6LMfMnuUFBx26JDY6nIZI9TEMB6_Or7rHz-dTfwmnWw2Cb_8qC7P5NbpYEPVb84WfIDC8tOD7OEpC5ntT8sO-VCCJIi4Jj08WU_eOSP_BgzmTN0xCvkpUEP5PLC5Dj4.bUMC2sJlUEjrDT6HRN4a5G0WARUltUquQj36qr4Q_xk&dib_tag=se&keywords=Reolink%2BRLK8-800B4&qid=1779720570&sr=8-3&ufe=app_do%3Aamzn1.fos.36393ef6-f0da-44a9-8c13-668db228262a&th=1&linkCode=ll2&tag=tisaseguridad-21&linkId=28ec48d723727703a8d6a482e6fece76&ref_=as_li_ss_tl",
         image: "https://m.media-amazon.com/images/I/61MceWFXjxL._AC_SL1500_.jpg",
         category: "CCTV Pack"
       },
@@ -137,7 +137,7 @@ export const solutionCategories: SolutionCategory[] = [
           "Integración Omada: Gestión centralizada en la nube si el negocio crece y añade más puntos de acceso o interruptores."
         ],
         whyRecommend: "Sé por mis experiencias que de nada sirve blindar la puerta de tu local si dejas la puerta digital abierta. Este router crea un muro de ciberseguridad que impide que pirateen tus cámaras desde internet. Además, su capacidad de gestionar dos líneas de internet a la vez es mi recomendación definitiva para que tu negocio nunca se quede desconectado ni pierda la vigilancia online.",
-        amazonUrl: "https://www.amazon.es/s?k=TP-Link+Omada+ER605+V2",
+        amazonUrl: "https://www.amazon.es/TP-LINK-Gigabit-Multi-WAN-Router-Ethernet/dp/B08MH4VLR3?dib=eyJ2IjoiMSJ9.HTU8g4U_-brTVKfIAoheHfeVWuH4ztkpTmqyhsOP4VJ-NoDniG0V3ELnYfobmxSA6NtCA6MWkPKLxmx4lmSDsdklbxhlLiSUxXWyzdvhyo17xSC_QbFKMrchkLaxhz44c69vx3KWEKTtqZvnbgY6zAJw-RLIQUIhkQEpsFjYejGuk6EuZJQXBpjKTVTh2_dykQvt9c6iK2gvv7QgtuMGsa1X4PMWAT714Oh4ESqARd_duXvYH1B-dO_1wlulG_HSck21YvtJi76LX_4-ASPyiAoqsoOIET-CBXmSJ6wtWPQ.BJvfSXOrCun1L1Mnf3ZRy8CcnYzd_YI-tyF26hhum_k&dib_tag=se&keywords=TP-Link+Omada+ER605+V2&qid=1779720625&sr=8-1&linkCode=ll2&tag=tisaseguridad-21&linkId=4032d8a504e14b573fce7970d9875712&ref_=as_li_ss_tl",
         image: "https://m.media-amazon.com/images/I/61qdLu4Hw2L._AC_SL1000_.jpg",
         category: "Networking"
       },
@@ -151,7 +151,7 @@ export const solutionCategories: SolutionCategory[] = [
           "Visión Nocturna DarkFighter: Captura imágenes nítidas en condiciones de oscuridad casi total donde otras cámaras solo ven negro."
         ],
         whyRecommend: "Esta cámara de Hikvision es, según mi opinión, de las mejores cuando se busca una protección perimetral estricta y profesional. Su analítica de vídeo con IA es extraordinaria: en lugar de hacerte perder horas buscando en las grabaciones qué ha pasado, mi criterio para recomendarla es su capacidad de filtrar los vídeos al instante mostrando solo los momentos donde detecta intrusos reales. Es la cámara que necesitas para proteger tus activos más valiosos.",
-        amazonUrl: "https://www.amazon.es/s?k=Hikvision+DS-2CD2186G2-I",
+        amazonUrl: "https://www.amazon.es/Camara-Domo-Ip-8Mp-Fija/dp/B08VWTHNBT?dib=eyJ2IjoiMSJ9.7vIHpMn4wqilXbfL_fvR_rCSH1ba3K83lYwiFtUQU4mNzhD_wEJ-eP8SzMkHpCbCW_1U-cYJyfiWsylmjoOCRtXeIUUtKFD_7o57snTtFlunW4H6poIjuKJBxQciFNWDJKJyeKyfv2t0O0wpraV69BjXi4LYt8ZAVsBUdDXSXvjDCmRXldeMS65HQMQGoubTllnDdrjXLzLYAAxXMKf0ytieGTuO8nd4_u_pwEdm0l0knP83KXC4yQTJpElvqU4odFrvmTgcaVCjMNjAcwXFx-r7FdO4S48F_0So5WSiD5k.cf-bzre-LKtsXCQ75CUQBAuRFqresRA45qD8Rt5g2U8&dib_tag=se&keywords=Hikvision+DS-2CD2186G2-I&qid=1779720675&sr=8-1&ufe=app_do%3Aamzn1.fos.4c3f56c3-e485-4a35-9abc-6532b61c3b62&linkCode=ll2&tag=tisaseguridad-21&linkId=ee5db8747da261d248d85a5ae7c9a0f6&ref_=as_li_ss_tl",
         image: "https://m.media-amazon.com/images/I/41QHN9yiRTL._AC_.jpg",
         category: "Cámara Pro"
       },
@@ -165,8 +165,8 @@ export const solutionCategories: SolutionCategory[] = [
           "Audio Bidireccional: Micrófono integrado de alta fidelidad para escuchar lo que ocurre en la zona de cajas o atención al cliente."
         ],
         whyRecommend: "Este modelo no solo protege, sino que aportar valor a la gestión de tu negocio. Recomiendo su instalación en la entrada principal de locales porque cumple una doble función perfecta: vigila el acceso con la máxima resolución y, simultáneamente, te proporciona métricas exactas de aforo y afluencia de clientes para ayudarte a tomar mejores decisiones comerciales.",
-        amazonUrl: "https://www.amazon.es/s?k=Reolink+RLC-810A",
-        image: "https://m.media-amazon.com/images/I/41QHN9yiRTL._AC_.jpg",
+        amazonUrl: "https://www.amazon.es/gp/aw/d/B08F7C37NZ?_encoding=UTF8&pd_rd_plhdr=t&aaxitk=31a3c4c1fa424fcfd0856e8d25a7aaeb&hsa_cr_id=0&qid=1779720830&sr=1-1-e0fa1fdd-d857-4087-adda-5bd576b25987&aref=xwrgWOjOg9&pd_rd_w=2xEgU&content-id=amzn1.sym.c89e522f-807e-4f97-a160-12864f02a095%3Aamzn1.sym.c89e522f-807e-4f97-a160-12864f02a095&pf_rd_p=c89e522f-807e-4f97-a160-12864f02a095&pf_rd_r=VCH8CSKCSWN6A5365VNS&pd_rd_wg=BP6vB&pd_rd_r=e8e7f440-efa2-4b58-be80-59e7d128f1c6&th=1&linkCode=ll2&tag=tisaseguridad-21&linkId=2597bccc7f8b75d7b6dbfa6ff2cff535&ref_=as_li_ss_tl",
+        image: "https://m.media-amazon.com/images/I/51KPV8w+SDL._AC_SL1500_.jpg",
         category: "Grabación"
       },
       {

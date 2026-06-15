d824d129cb74b36290d2902ee3119295d5cfe32e
Websprint <websprintt@gmail.com>
2026-06-15 19:16:34 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Seguridad-TISA/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
